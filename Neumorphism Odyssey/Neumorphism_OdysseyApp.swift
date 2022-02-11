39c2ba0d590f559885a1b3193bce193ff5f17374
Simbarashe Dombodzvuku <supernovaonline@outlook.com>
2022-02-11 00:28:30 +0000

    Initial Commit

---
+++ Neumorphism Odyssey/Neumorphism_OdysseyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neumorphism_OdysseyApp.swift
+//  Neumorphism Odyssey
+//
+//  Created by user213454 on 2/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Neumorphism_OdysseyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
